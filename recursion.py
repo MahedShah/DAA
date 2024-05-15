7325828cb2e1c43023e210f4c5230537e6cb4e68
Syed Mahed Ahmed <99177018+MahedShah@users.noreply.github.com>
2024-05-15 15:58:52 +0500

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,12 @@
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return fact(n-1) * n
+
+
+def fib(n):
+    if n < 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
